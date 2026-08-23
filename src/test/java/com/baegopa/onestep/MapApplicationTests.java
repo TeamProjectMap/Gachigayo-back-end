2cb2020a2861d57e85bb7ac444fa011e0413d390
data8320-22 <rlaehddnwkd1107@gmail.com>
2026-08-23 14:41:43 +0900

    chore: 프로젝트 초기 구성

---
+++ src/test/java/com/baegopa/onestep/MapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baegopa.onestep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
